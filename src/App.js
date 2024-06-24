fe770a0c207ae238a0cee9b7c75781720bf509cb
infolanze807 <techteam.infolanze@gmail.com>
2024-06-24 15:35:17 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello This Is Demo Of <abbr title="Progressive Web App">PWA</abbr></h1>
+      <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTy3fSM7P-xtALrDQGmYm8HQA-a71lBi4cdpQ&s'/>
+    </div>
+  );
+}
+
+export default App;
