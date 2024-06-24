3bbc89041e397363a34d3e50496b4cc847635abd
infolanze807 <techteam.infolanze@gmail.com>
2024-06-24 15:48:30 +0530

    second

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ function App() {
   return (
     <div className="App">
       <h1>Hello This Is Demo Of <abbr title="Progressive Web App">PWA</abbr></h1>
-      <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTy3fSM7P-xtALrDQGmYm8HQA-a71lBi4cdpQ&s'/>
     </div>
   );
 }
